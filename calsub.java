781ea29e4248ee78aa0ab457a8af4f1a4bd102da
Rishabh Jain <45866679+alchemaniac@users.noreply.github.com>
2019-09-30 21:02:38 -0700

    added type conversion

--- calsub.java
+++ calsub.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class calsub{
         public static void main(String args[]){
                 float a,b,res;
-                int choice=args[0];
+                int choice=Integer.parseInt(args[0]);
                 do{
                 System.out.println("1. SUBTRACT TWO NUMBERS");
                 System.out.println("2. EXIT");
                 System.out.println("Enter choice: ");
                 switch(choice){
                         case '1': System.out.println("Input two numbers: ");
-                                  a=args[1];
-                                  b=args[2];
+                                  a=Float.parseFloat(args[1]);
+                                  b=Float.parseFloat(args[2]);
                                   res=a-b;
                                   System.out.println("Result is "+res);
                                   break;
